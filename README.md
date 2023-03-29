90eb48b617bcaf149ca687c26143203aaa387878
深海里有坏龙龙 <seele.peng@gmail.com>
2023-03-29 21:32:25 +0300

    📝 Add readme and IaC blueprint

---
+++ README.md
@@ -0,0 +1,39 @@
+<div align="center">
+
+# Todo
+
+The REST Todo!
+
+</div>
+
+## Description
+
+![preview](docs/preview.png)
+
+[![Deploy to Render](https://render.com/images/deploy-to-render-button.svg)][5]
+
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the web service in the development mode.\
+Api at [http://localhost:3000](http://localhost:3000) including:
+
+- "/" get: all data from database.
+- "/" post: add data to database.
+- "/:id" delete: remove a data from database.
+
+### `npm build`
+
+Build the static page for production to the 'dist' folder.\
+
+The build is minified and the filenames include the hashes.\
+
+### `npm devlop`
+
+Run webpack development server for easy development.
+
+[5]: https://render.com/deploy
--- server/configs/postgresql.ts
+++ server/configs/postgresql.ts
@@ -1,10 +1,22 @@
-import { PoolConfig } from "pg"
+import {PoolConfig} from "pg"
 
-export const config: PoolConfig = {
-    user: process.env["db_user"] ?? "postgres",
-    host: process.env["db_host"] ?? "localhost",
-    database: process.env["db_database"] ?? "todo",
-    password: process.env["db_password"] ?? "root",
-    port: +(process.env["db_port"] ?? 5432),
-    ssl: Boolean(process.env["db_ssl"])
+const {
+    db_user,
+    db_database,
+    db_password,
+    db_port,
+    db_host,
+    db_connectionString,
+    db_ssl
+} = process.env;
+
+export const config: PoolConfig = db_connectionString ? {
+    connectionString: db_connectionString
+} : {
+    user: db_user ?? "postgres",
+    database: db_database ?? "todo",
+    password: db_password ?? "root",
+    port: +(db_port ?? 5432),
+    host: db_host ?? "localhost",
+    ssl: Boolean(db_ssl)
 }
